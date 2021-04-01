c33d2b8fa288975bad744ac49e7e621402eea3d0
RanaShubham <ranashubham7296@gmail.com>
2021-04-01 22:13:11 +0530

    Hurray git add . Search working now :D

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import './App.css';
 import React,{useEffect, useState} from 'react';
+import Recipe from './Recipe';
 
 const App = () => {
 
-  const APP_ID  = "66216237";
-  const APP_KEY = "a4946b48045d3f08510d23436ff1e137";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const APP_ID  = "2e17dc01";
+  const APP_KEY = "eb394a7007cd98a44599a1118c3099a0";
+  // var exampleReq = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const[counter, setCounter] = useState(0);
-  
+  const [counter, setCounter] = useState(0);
+  const [recipies, setRecipies] = useState([]); //state to store recipies.
+  const [search, setSearch] = useState(''); //state to store the incomplete search term while it's being written before clicking on search button.
+  const [query, setQuery] = useState('paneer'); //state to store the final search term.
+    
   useEffect( () => {
-    alert("effect ran!!");
-  }, [counter]);
+    getRecepies();
+  }, [query]);
+
+  const getSearch = e => {
+    e.preventDefault(); //To stop the automatic page refresh everytime we start typing into the search box.
+    setQuery(search);
+    setSearch('');
+  }
+
+  const getRecepies = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipies(data.hits);
+
+    // Alternate way.
+    // fetch(exampleReq)
+    // .then(response => {
+    //   response.json
+    // })
+  }
+  
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
 
   return(
     <div className = "App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
-        <button className="search-button" onClick={(event)=>event.preventDefault()} type="submit">Search</button>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit">Search</button>
       </form>
+      {recipies.map(x => ( //Here we have map function which takes another function to apply over all the elemets in recepies array.
+        <Recipe 
+        title={x.recipe.label} 
+        calories={x.recipe.calories} 
+        image={x.recipe.image} 
+        key={x.recipe.label} /> // For the inner function we are just defining the return and not the body hence () instead of {} after recipe => ()
+      ))}
       <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
     </div>
   )
