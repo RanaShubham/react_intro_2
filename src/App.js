6099cf273953887d0e2f14bed3ff327987b9367a
RanaShubham <ranashubham7296@gmail.com>
2021-04-01 11:24:36 +0530

    [Shubham Rana] Added: Edmam id and key to App.js.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const APP_ID  = "66216237";
+  const APP_KEY = "a4946b48045d3f08510d23436ff1e137";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
+
+  return(
+    <div className = "App">
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
