b7923aeccd985398eceaf497f8e91d735276d876
RanaShubham <ranashubham7296@gmail.com>
2021-04-01 11:32:29 +0530

    Added: A search button using and form to take input.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ const App = () => {
   const APP_KEY = "a4946b48045d3f08510d23436ff1e137";
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-
-
   return(
     <div className = "App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   )
 }
